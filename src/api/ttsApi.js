beeb66108360e5a822dfe9b7eca34a53cb95dd37
Rom Jr. <157264890+RomNeedBoba@users.noreply.github.com>
2026-05-04 07:34:16 +0700

    Update ttsApi.js

--- src/api/ttsApi.js
+++ src/api/ttsApi.js
@@ -1,5 +1,5 @@
 // Pointing to your Node.js Proxy
-const API_BASE_URL = "https://meals-discussion-forests-crawford.trycloudflare.com/api";
+const API_BASE_URL = "https://revenue-fellowship-amend-cultures.trycloudflare.com/api";
 
 export const startAudioGeneration = async (text, model, voice) => {
   const response = await fetch(`${API_BASE_URL}/generate`, {
