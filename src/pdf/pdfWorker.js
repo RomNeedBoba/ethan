6f27b273723b2bdae6f97ae95534acd95b03d0df
RomNeedBoba <rpphyrom.dev@gmail.com>
2026-05-27 07:52:30 +0700

    so messeduo

--- src/pdf/pdfWorker.js
+++ src/pdf/pdfWorker.js
@@ -1,4 +1,14 @@
-import { pdfjs } from "react-pdf";
-import workerSrc from "pdfjs-dist/build/pdf.worker.min.mjs?url";
+import * as pdfjsLib from 'pdfjs-dist';
 
-pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
+// If you are using pdfjs-dist v4.x (Modern):
+pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.mjs', // try .mjs instead of .min.mjs
+  import.meta.url
+).toString();
+
+// ----------------------------------------------------
+// OR, if you are using pdfjs-dist v3.x (Older):
+// pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
+//  'pdfjs-dist/build/pdf.worker.min.js', 
+//  import.meta.url
+// ).toString();
